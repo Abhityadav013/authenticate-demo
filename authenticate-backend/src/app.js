214df627d7e01cb2cc5e5cd7e6ffe42b207af68d
Abhityadav013 <abhityadav013@gmail.com>
2025-02-05 04:26:05 +0530

    fix cors issues

--- authenticate-backend/src/app.js
+++ authenticate-backend/src/app.js
@@ -68,7 +68,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://testing.indiantadka.eu", "http://localhost:3000"],
     credentials: true, // Allows cookies to be sent and received
     methods: ['GET', 'POST', 'PUT','DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
