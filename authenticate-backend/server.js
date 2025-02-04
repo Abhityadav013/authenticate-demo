e463da8efe9be93e2228640eb2a2d3d34ca8241f
Abhityadav013 <abhityadav013@gmail.com>
2025-02-05 00:14:25 +0530

    fix vercel deployment issue

---
+++ authenticate-backend/server.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import connectDB from "./src/config/mongodb.js";
+import app from "./src/app.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
--- authenticate-backend/src/app.js
+++ authenticate-backend/src/app.js
@@ -1,45 +1,45 @@
-// import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUi from "swagger-ui-express";
-// import authRouter from "./users/routes/auth.route.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import authRouter from "./users/routes/auth.route.js";
 
-// const app = express();
+const app = express();
 
-// const swaggerOptions = {
-//   swaggerDefinition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Authentication",
-//       version: "1.0.0",
-//       description: "API documentation for Authentication",
-//     },
-//     servers: [
-//       {
-//         url: `http://localhost:${process.env.PORT || 5000}`|| 'https://authenticate-demo.vercel.app/api/v1',
-//       },
-//     ],
-//   },
-//   apis: ["./src/users/routes/*.js"], // path to your API routes (adjust as necessary)
-// };
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Authentication",
+      version: "1.0.0",
+      description: "API documentation for Authentication",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 5000}`|| 'https://authenticate-demo.vercel.app/api/v1',
+      },
+    ],
+  },
+  apis: ["./src/users/routes/*.js"], // path to your API routes (adjust as necessary)
+};
 
-// const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-// // Use Swagger UI middleware
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// Use Swagger UI middleware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// app.get('/', (req, res) => {
-//   res.redirect('/api-docs'); // Redirect to Swagger UI
-// });
+app.get('/', (req, res) => {
+  res.redirect('/api-docs'); // Redirect to Swagger UI
+});
 
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({
-//   origin: ['https://testing.indiantadka.eu/','http://localhost:3000'],
-//   credentials: true, // Allows cookies to be sent and received
-// }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: ['https://testing.indiantadka.eu/','http://localhost:3000'],
+  credentials: true, // Allows cookies to be sent and received
+}));
 
-// app.use('/api/v1', authRouter)
+app.use('/api/v1', authRouter)
 
-// export default app
+export default app
